ef70194bea33bf381ed771740f6f6db90c3de241
elayne <elacheu@gmail.com>
2023-10-14 15:27:18 -0500

    fiveday and current functions working

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,67 @@
+var form = $('#searchForm');
+var submitButton = $('.btn');
 
-var APIKey = "43c04c5c5205b5bc0cbf1dfb25c37cf3";
-
-
-
-searchButton.click(function() {
-    var searchInput = $(".searchInput").val();
-
+form.on("submit", function(event) {
+    event.preventDefault(); 
+    console.log(event);
+    var city = event.target[0].value;
+    getApi(city);
 })
 
+var apiKey = "43c04c5c5205b5bc0cbf1dfb25c37cf3";
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// a weather dashboard with form inputs
+// search for a city
+
+function getApi(city) {
+    var queryUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
+    fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getFiveDay(lat,lon);
+      getCurrent(lat,lon);
+    
+})
+}
+
+function getFiveDay(lat,lon) {
+  var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+  fetch(queryUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  
+})
+}
+
+function getCurrent(lat,lon) {
+  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+  fetch(queryUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  
+})
+}
+
+/*
+presented with current and future conditions for that city and that city is added to the search history
+view current weather conditions for that city
+
+presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+view future weather conditions for that city
+
+presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+click on a city in the search history
+
+presented with current and future conditions for that city
+*/
