392b3d26eaa3658af5d2bb7f12d1716640a53466
elayne <elacheu@gmail.com>
2023-10-27 21:55:11 -0500

    search list displaying and functional, partial functioning local storage

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,55 @@
 var form = $('#searchForm');
 var submitButton = $('.btn');
 
-
 // form click event to search for city
 form.on("submit", function(event) {
     event.preventDefault(); 
     var city = event.target[0].value;
     getApi(city);
 
-})
+    handleSearch();
+ })
 
-var apiKey = "43c04c5c5205b5bc0cbf1dfb25c37cf3";
+// save to local storage and create functional displayed search list
+
+ var searchHistory = JSON.parse(localStorage.getItem('savedCity')) || [];
+
+ function handleSearch() {
+  var city = document.getElementById('searchInput').value;
+
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+
+    localStorage.setItem('savedInput', JSON.stringify(city));
+
+    updateSearchHistoryUI();
+    console.log(city)
+  }
+}  
+
+console.log(searchHistory)
+ function updateSearchHistoryUI() {
+  var searchHistoryList = document.getElementById('search-history');
+  searchHistoryList.innerHTML = "";
+
+  for (var i = 0; i < searchHistory.length; i++) {
+    var cityButton = document.createElement('button');
+    cityButton.textContent = searchHistory[i];
+    cityButton.addEventListener('click', function() {
+      var city = this.textContent;
+      getApi(city);
+      
+    });
+    searchHistoryList.appendChild(cityButton);
+  }
+ }
+
+ updateSearchHistoryUI();
+
 
 // get api function to pull longitute and latitude location of searched city
+var apiKey = "43c04c5c5205b5bc0cbf1dfb25c37cf3";
+
 function getApi(city) {
     var queryUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
     fetch(queryUrl)
@@ -29,32 +66,30 @@ function getApi(city) {
 })
 }
 
+
 // function for 5 day forecast
 function getFiveDay(lat,lon) {
-  var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
-  fetch(queryUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(data => {
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+    fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(data => {
     console.log(data);
     let forecastContainer = document.getElementById('forecast-container');
-    let forecastData = data.list.slice(0,5);
-    console.log(forecastData)
    
-    for (let i = 0; i < forecastData.length; i++) {
-      let forecast= forecastData[i];
-
-
-    forecastData.forEach(forecast => { 
+    for (let i = 0; i < data.list.length; i+=8) {
+    console.log(data.list[i])
+   
     let day = data.list[i].dt_txt;
     let name = data.city.name;
-    let weatherIcon = data.list[i].weather[i].icon;
+    let weatherIcon = data.list[i].weather[0].icon;
     let weatherIconUrl = 'https://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
     let temp = data.list[i].main.temp;
     let humidity = data.list[i].main.humidity;
     let windSpeed = data.list[i].wind.speed;
 
+    
 
     let forecastCard = document.createElement('div');
       forecastCard.classList.add('forecastCard');
@@ -81,10 +116,11 @@ function getFiveDay(lat,lon) {
     
     displayCard.appendChild(forecastCard);
     forecastContainer.appendChild(displayCard);
+  
+      }
     })
-    }
-  })
-  }
+  };
+
 
 // function for current day weather 
 function getCurrent(lat,lon) {
@@ -94,59 +130,22 @@ function getCurrent(lat,lon) {
     return response.json();
   })
   .then(data => {
-  let cityName = data.name;
-  let weatherIcon = data.weather['0'].icon;
-  let weatherIconUrl = 'https://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
-  let temp = data.main.temp;
-  let humidity = data.main.humidity;
-  let windSpeed = data.wind.speed;  
-  city.innerHTML = cityName;
-  currentIcon.src = weatherIconUrl;
-  temperature.innerHTML = 'Temperature: ' + temp;
-  humidityEl.innerHTML = 'Humidity: ' + humidity;
-  wind.innerHTML = 'Wind Speed: ' + windSpeed;
+    let cityName = data.name;
+    let weatherIcon = data.weather['0'].icon;
+    let weatherIconUrl = 'https://openweathermap.org/img/wn/' + weatherIcon + '@2x.png';
+    let temp = data.main.temp;
+    let humidity = data.main.humidity;
+    let windSpeed = data.wind.speed;  
+    city.innerHTML = cityName;
+    currentIcon.src = weatherIconUrl;
+    temperature.innerHTML = 'Temperature: ' + temp;
+    humidityEl.innerHTML = 'Humidity: ' + humidity;
+    wind.innerHTML = 'Wind Speed: ' + windSpeed;
   
 
-// save search inputs to local storage
-
-  var weatherData = {
-    city: data.name,
-    temp: data.main.temp,
-    humidity: data.main.humidity,
-    windSpeed: data.wind.speed,
-    
-  }
-  let weatherDataString = JSON.stringify(weatherData);
-  localStorage.setItem('weatherData', weatherDataString);
-
-  // display search history
-  var savedSearch = JSON.parse(localStorage.getItem("weatherData"));
-  const displayList = document.getElementById('search-history');
-  const listItem = document.createElement('li');
-  const button = document.createElement('button');
-  button.textContent = data.name;
-  listItem.appendChild(button);
-  displayList.appendChild(listItem)
-
-  const searchList = document.getElementById('button');
-  searchList.addEventListener('click', fetchData) 
-  function fetchData() {
-  getApi();
-  getFiveDay();
-  console.log(searchList);
-    }
   })
-}
-
-/*
-click on a city in the search history
-presented with current and future conditions for that city
-
-local storage, searched cities persist on the page
-*/
-
-/////////
-
-// limit for loop for 5 day / problem: only display one day
-// functional search list
-// convert date/time display
+};
+  
+// not pushing into array in local storage
+// clear previous search 
+// data persist
